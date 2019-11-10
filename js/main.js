8fcec4df128c3639beafd5adbd9c8ac884b66962
arrooney <arrooney@ualberta.ca>
2019-11-09 21:50:46 -0700

    adding testing the cursors

--- js/main.js
+++ js/main.js
@@ -19,9 +19,21 @@ function preload ()
 
 function create () {
     // Send the server a newplayer event
+    
     Client.askNewPlayer();
 }
 
-function update ()
-{
+function update () {
+    cursors = this.input.keyboard.createCursorKeys();
+    if (cursors.left.isDown) {
+        console.log("Left");
+    }
+    else if (cursors.right.isDown) {
+        console.log("Right");
+    }
+    else if (cursors.up.isDown || cursors.space.isDown){
+        console.log("jump");
+    } else {
+        console.log("nothing");
+    }
 }
