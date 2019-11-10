82c1b49fc29d0c91b2c5c1cfd891ce59a8a8396d
arrooney <arrooney@ualberta.ca>
2019-11-09 20:11:01 -0700

    adding /js and small main function

---
+++ js/main.js
@@ -0,0 +1,26 @@
+console.log("Creating new phaser game...");
+
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+}
+
+function create ()
+{
+}
+
+function update ()
+{
+}
